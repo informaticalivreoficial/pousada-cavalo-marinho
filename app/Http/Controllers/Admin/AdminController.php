02ff0b29b8ccea4a053956dbb52757e19d06579f
informaticalivreoficial <renatomontanari@ymail.com>
2025-04-06 23:07:16 -0300

    fix analitcs

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -73,17 +73,17 @@ class AdminController extends Controller
                 ->sum('views');
 
         //Analitcs
-        $visitasHoje = Analytics::fetchMostVisitedPages(Period::days(1));
+        // $visitasHoje = Analytics::fetchMostVisitedPages(Period::days(1));
         
-        $visitas365 = Analytics::fetchTotalVisitorsAndPageViews(Period::months(5));
+        // $visitas365 = Analytics::fetchTotalVisitorsAndPageViews(Period::months(5));
         
-        $top_browser = Analytics::fetchTopBrowsers(Period::months(5), 10);
+        // $top_browser = Analytics::fetchTopBrowsers(Period::months(5), 10);
 
-        $analyticsData = Analytics::get(
-                Period::months(6), 
-                metrics: ['totalUsers', 'sessions', 'screenPageViews'], 
-                dimensions: ['month']
-        );
+        // $analyticsData = Analytics::get(
+        //         Period::months(6), 
+        //         metrics: ['totalUsers', 'sessions', 'screenPageViews'], 
+        //         dimensions: ['month']
+        // );
 
         //dd($analyticsData);
          
@@ -116,10 +116,10 @@ class AdminController extends Controller
             'postsNoticias' => $postsNoticias,
             'postsPaginas' => $postsPaginas,
             //Analytics
-            'visitasHoje' => $visitasHoje,
+            //'visitasHoje' => $visitasHoje,
             //'visitas365' => $visitas365,
-            'analyticsData' => $analyticsData,
-            'top_browser' => $top_browser
+            //'analyticsData' => $analyticsData,
+            //'top_browser' => $top_browser
         ]);
     }
     
